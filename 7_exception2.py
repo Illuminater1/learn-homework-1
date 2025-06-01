a6533f26516fcbed6bc9639c6584cbed0be7f19a
Illuminater1 <romashcn@gmail.com>
2025-06-01 23:27:14 +0300

    7 - solved

--- 7_exception2.py
+++ 7_exception2.py
@@ -10,15 +10,30 @@
 * Первые два нужно приводить к вещественному числу при помощи float(),
   а третий - к целому при помощи int() и перехватывать исключения
   ValueError и TypeError, если приведение типов не сработало.
-    
+
 """
 
-def discounted(price, discount, max_discount=20)
+def discounted(price, discount, max_discount=20):
     """
     Замените pass на ваш код
     """
-    pass
-    
+    try:
+        price = abs(float(price))
+        discount = abs(float(discount))
+        max_discount = abs(int(max_discount))
+    except ValueError:
+        return ("Введено некоректное значение")
+    except TypeError:
+        return ("Ввeденное значение невозможно преобразовать")
+
+    if max_discount >= 100:
+        raise ValueError("Максимальная скидка не должна быть больше 100")
+    if discount >= max_discount:
+        price_with_discount = price
+    else:
+        price_with_discount = price -(price * discount / 100)
+    return price_with_discount
+
 if __name__ == "__main__":
     print(discounted(100, 2))
     print(discounted(100, "3"))
